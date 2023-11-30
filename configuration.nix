0111a6993956c8bd9362805997ac8db3142593d7
Chell L <pumpkinbiscuit@protonmail.com>
2023-11-30 09:16:52 -0500

    packages, fonts, and script variables (still WIP) the scripts don't seem to work rn, but maybe after a restart, idk i'm going to sleep

--- configuration.nix
+++ configuration.nix
@@ -59,21 +59,21 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = false;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+  # };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -102,6 +102,7 @@
     wl-clipboard
     nodejs
     signal-desktop
+    discord
     jq
     bc # weather.sh dep
     keepassxc
--- flake.nix
+++ flake.nix
@@ -9,30 +9,6 @@
   outputs = { self, nixpkgs, ... }:
   let
     lib = nixpkgs.lib;
-    # dbus-sway-environment = pkgs.writeTextFile {
-    #   name = "dbus-sway-environment";
-    #   destination = "/bin/dbus-sway-environment";
-    #   executable = true;
-
-    #   text = ''
-    #     dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-    #     systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    #     systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    #   '';
-    # };
-    # configure-gtk = pkgs.writeTextFile {
-    #   name = "configure-gtk";
-    #   destination = "/bin/configure-gtk";
-    #   executable = true;
-    #   text = let
-    #     schema = pkgs.gsettings-desktop-schemas;
-    #     datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    #   in ''
-    #     export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-    #     gnome_schema=org.gnome.desktop.interface
-    #     gsettings set $gnome_schema gtk-theme 'Dracula'
-    #   '';
-    # };
   in {
     nixosConfigurations = {
       daily = lib.nixosSystem {
@@ -42,44 +18,84 @@
 
           (./configuration.nix)
 
-          ({ pkgs, ... }: {
+          ({ pkgs, ... }:
+          let
+            dbus-sway-environment = pkgs.writeTextFile {
+              name = "dbus-sway-environment";
+              destination = "/bin/dbus-sway-environment";
+              executable = true;
 
+              text = ''
+                dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+                systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+                systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+              '';
+            };
+            configure-gtk = pkgs.writeTextFile {
+              name = "configure-gtk";
+              destination = "/bin/configure-gtk";
+              executable = true;
+              text = let
+                schema = pkgs.gsettings-desktop-schemas;
+                datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+              in ''
+                export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+                gnome_schema=org.gnome.desktop.interface
+                gsettings set $gnome_schema gtk-theme 'Dracula'
+              '';
+            };
+          in {
             environment.systemPackages = with pkgs; [
-              # cmatrix
+              cmatrix
+              ranger
+              zathura
               vivaldi
-              alacritty
-              kitty
+              cinnamon.nemo # files
+              alacritty # terminal :3
+              kitty # terminal<3
               wayland
               xwayland
               xdg-utils
               glib
-              # dracula-theme
-              # gnome3.adwaita-icon-theme
+              dracula-theme
+              gnome3.adwaita-icon-theme
               swaylock
               swayidle
-              grim
-              slurp
+              slurp # screenshot pt1
+              grim # screenshot pt2, the screenshot returns (to your ~/Pictures/Screenshots folder)
               wl-clipboard
-              bemenu
-              mako
+              bemenu # wayland dmenu
+              mako # notif daemon
+              libnotify # includes notify-send
               wdisplays
               # dunst
               i3status-rust
               pulseaudio
-              # dbus-sway-environment
-              # configure-gtk
+              libwebp
+              dbus-sway-environment
+              configure-gtk
             ];
 
-            fonts.fonts = with pkgs; [
-              iosevka
-              # noto-fonts
-              # noto-fonts-cjk
-              # noto-fonts-extra
-              # noto-fonts-emoji
-              noto-fonts-lgc-plus
-              font-awesome
-              font-awesome_4
-            ];
+            fonts = {
+              enableDefaultFonts = true;
+              fonts = with pkgs; [
+                iosevka
+                noto-fonts
+                noto-fonts-cjk
+                noto-fonts-extra
+                noto-fonts-emoji
+                noto-fonts-lgc-plus
+                font-awesome
+                font-awesome_4
+                takao
+              ];
+            };
+
+            services.pipewire = {
+              enable = true;
+              alsa.enable = true;
+              pulse.enable = true;
+            };
 
             services.dbus.enable = true;
             xdg.portal = {
